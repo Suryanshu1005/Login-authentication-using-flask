eca5e2914f5981691d2086423ddf87a42d48d89e
Suryanshu <tshuryanshu@gmail.com>
2022-08-23 13:26:39 +0530

    new changes

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash 
+from . import db
+
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
     return render_template("Login.html")
 
@@ -10,6 +15,29 @@ def login():
 def logout():
     return "<p> logout </p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_name) < 3:
+            flash('First Name must be greater than 3 characters.', category='error')
+        elif len(password1) < 6:
+            flash('Password must be greater than 6 characters.', category='error')
+        elif len(password2) < 6:
+            flash('Confirm Password must be greater than 6 characters.', category='error')
+        elif password1 != password2:
+            flash('Password must be same', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password = generate_password_hash(password1, method = 'sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("signup.html")
