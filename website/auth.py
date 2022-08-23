07ebfcdf41bdd6b100a5e87f6cc82551d8bff17e
Suryanshu <tshuryanshu@gmail.com>
2022-08-23 14:25:12 +0530

    new changes on 23/08

--- website/auth.py
+++ website/auth.py
@@ -1,19 +1,34 @@
+import email
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash 
 from . import db
-
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template("Login.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(url_for('views.home'))
+            else:
+                flash('incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 def logout():
-    return "<p> logout </p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
